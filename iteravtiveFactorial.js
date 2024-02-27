c5eb1058c782c2fce83ae3404b9dbd4d511f1932
haider-codes23 <haide.codes23@gmail.com>
2024-02-27 09:37:55 +0500

    updaing content of file

--- iteravtiveFactorial.js
+++ iteravtiveFactorial.js
@@ -12,15 +12,3 @@ function factorialIteratively(num) {
 // console.log(factorial(3)); // 6
 
 
-// factorail recursively
-function factorialRecursivly(num) {
-  if (num === 1)  return 1; // This is the base case
-  return num * factorialRecursivly(num - 1); // this is calling the function recursively with a smaller or different input 
-                    // return 3 * factorialRecursively(2)
-                    //               return 2 * factorialRecursivly(1)
-  }
-  
-
-
-console.log(factorialRecursivly(3));
-console.log(factorialRecursivly(4));
