948a89b3af556e2ac895bdff57c6a35977c51e82
haider-codes23 <haide.codes23@gmail.com>
2024-03-31 00:17:04 +0500

    uploading code for insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,22 @@
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    console.log("entered outer loop")
+    let unsorted = i + 1;
+    
+    for (let j = i + 1; j > 0; j--) {
+      console.log(`J: ${j} , before Comparing: ${arr} , comapring ${arr[j]} is less than ${arr[j - 1]}`);
+      if (arr[j] < arr[j - 1]) {
+        console.log("Swaping!!!");
+        [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+        console.log(`After Swapping: ${arr}`);
+      }
+      
+      
+
+    }
+    console.log("***********************************");
+  }
+  return arr;
+}
+
+console.log(insertionSort([5, 3, 4, 1, 2]));
