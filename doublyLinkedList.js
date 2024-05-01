26ab5e67fbd995ec33b0db7cfcf8b99da33688cf
haider-codes23 <haide.codes23@gmail.com>
2024-05-01 17:57:34 +0500

    implemented pop instance method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -28,6 +28,26 @@ class DoublyLinkedList {
     return this;
   }
 
+  // implementing pop instance methods
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev =null;
+
+    }
+    this.length--;
+    return poppedNode;
+
+  }
+
 
 
 
