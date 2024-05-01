ca3359e700bb4078e631b019498df7e1508f8062
haider-codes23 <haide.codes23@gmail.com>
2024-05-01 16:48:19 +0500

    implemented push instance method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -12,4 +12,30 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  // implementing push instance method
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+
+
+
 }
+
+let list = new DoublyLinkedList();
+list.push(1)
+list.push(2)
+console.log(list.push(3));
+
+
