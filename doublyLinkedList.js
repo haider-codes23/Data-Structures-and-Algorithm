a594f9b3cc82290a3e3c4b051247b3d535ca1dd7
haider-codes23 <haide.codes23@gmail.com>
2024-05-01 18:44:52 +0500

    implemented instance method shift

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -45,7 +45,22 @@ class DoublyLinkedList {
     }
     this.length--;
     return poppedNode;
+  }
 
+  // implementing shift method
+  shift() {
+    if (this.length === 0) return undefined;
+    let removedNode = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail =null;
+    } else {
+      this.head = removedNode.next;
+      removedNode.next = null;
+      this.head.prev = null;
+    }
+    this.length--;
+    return removedNode;
   }
 
 
