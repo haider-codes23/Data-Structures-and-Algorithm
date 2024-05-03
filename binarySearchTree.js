f24363b21e22ec3a351f04acf1ed36785bb0e446
haider-codes23 <haide.codes23@gmail.com>
2024-05-03 19:25:47 +0500

    implemented Binary serach tree find instance method and insert method

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -15,4 +15,90 @@ class BinarySearchTree {
     // When we first create a binary search tree it doesn't have any node so we initalize the root node to null
     this.root = null;
   }
+
+  // implenting insert instance method
+  // We will pass a value to the insert method it will create a new node and insert it in the correct spot in the tree
+  insert (val) {
+    //create a new node
+    let newNode = new Node(val);
+    // check if there is a root node
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      // IF there is a root check if the value of the new node is greater than or less than the value of the root node
+      let current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else  if (val > current.val){
+            if (current.right === null) {
+              current.right = newNode;
+              return this;
+            } else {
+              current = current.right;
+            }
+        }
+      }
+    }
+  }
+
+  // implementing find method
+  // It finds a node if it exists it return true, if it doesn't it returns false
+  find(val) {
+    if (!this.root) {
+      return false;
+    } else if (this.root.val === val) {
+      return true;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val < current.val) {
+          if (current.left === null) {
+            return false;
+          } else if (current.left.val === val) {
+            return true;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+            if (current.right === null) {
+              return false;
+            } else if (current.right.val === val) {
+              return true;
+            } else {
+              current = current.right;
+            }
+        }
+      }
+    }
+    
+  }
+
+
+
+
+
 }
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(9);
+tree.insert(13);
+tree.insert(20);
+
+console.log(tree.find(21))
+
+
+
+
