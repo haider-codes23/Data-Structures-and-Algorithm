ef28c78119f4d9d85f1108347f050f10e204e27f
haider-codes23 <haide.codes23@gmail.com>
2024-05-07 12:11:15 +0500

    implemented extractMax method for maxBinarySearch

--- maxBinaryHeaps.js
+++ maxBinaryHeaps.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeaps {
   constructor() {
-    this.values = [];
+    this.values = [55, 39, 41, 18, 27, 12, 33];
   }
 
   //bubble Up method
@@ -26,11 +26,55 @@ class MaxBinaryHeaps {
     this.values.push(element);
     this.bubbleUp();
   }
+
+  // implementing extractMax that removes the max element from the heap
+  extractMax () {
+    [this.values[0], this.values[this.values.length - 1]] = [this.values[this.values.length - 1], this.values[0]];
+    let length = this.values.length;
+    let maxElement = this.values.pop();
+    let parentIndex = 0;
+    let parentElement  = this.values[parentIndex];
+    while (true) {
+      let swap = false;
+      let leftChildIndex = 2 * (parentIndex) + 1;
+      let leftChild;
+      if (leftChildIndex < length){
+        leftChild = this.values[leftChildIndex];
+      }
+     
+  
+      let rightChildIndex = 2 * (parentIndex) + 2;
+      let rightChild;
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+      }
+  
+      if (leftChild > parentElement && leftChild > rightChild) {
+        [this.values[parentIndex], this.values[leftChildIndex]] = [this.values[leftChildIndex], this.values[parentIndex]];
+        parentIndex = leftChildIndex;
+        swap = true;
+      } else if (rightChild > parentElement && rightChild > leftChild) {
+        [this.values[parentIndex], this.values[rightChildIndex]] = [this.values[rightChildIndex], this.values[parentIndex]];
+        parentIndex = rightChildIndex;
+        swap = true;
+      }
+      if (swap === false) break; 
+
+    }
+
+    return maxElement;
+
+
+    
+  }
 }
 
 let heap = new MaxBinaryHeaps();
-heap.insert(41);
+// heap.insert(41);
 
-heap.insert(39);
+// heap.insert(39);
+console.log(heap);
+console.log("******************");
+heap.extractMax();
 console.log(heap);
 // [41, 39, 55, 18, 27, 12, 33];
